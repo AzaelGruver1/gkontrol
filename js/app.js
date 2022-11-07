66546482ace3c4383f51cdb29dd8d9ae1c11f287
Azael Garcia Jaimes <agarciaj@gruver.mx>
2022-11-06 21:31:30 -0600

    :

--- js/app.js
+++ js/app.js
@@ -83,6 +83,21 @@ function scrollNavegacion() {
 }
 scrollNavegacion()
 
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
 $(document).ready(function () {
     $('.customer-logos').slick({
         slidesToShow: 6,
